d549399c0ad0b8828189cd4d3c9696807a1eff50
Gulie <gulifeiya@gmail.com>
2022-07-24 18:11:10 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/library/step_definitions/Login_StepDefinition.java
@@ -0,0 +1,34 @@
+package com.cydeo.library.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinition {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @When("user enters librarian username")
+    public void userEntersLibrarianUsername() {
+    }
+
+    @And("user enters librarian password")
+    public void userEntersLibrarianPassword() {
+    }
+
+    @Then("user should see the dashboard")
+    public void userShouldSeeTheDashboard() {
+    }
+
+    @When("user enters student username")
+    public void userEntersStudentUsername() {
+    }
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
+    }
+}
---
+++ src/test/java/com/cydeo/library/utilities/Driver.java
@@ -0,0 +1,59 @@
+package com.cydeo.library.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){
+
+    }
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+
+
+
+    public static WebDriver getDriver(){
+        if (driverPool.get() == null) {
+
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType.toLowerCase()) {
+
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+            }
+
+        }
+        return driverPool.get();
+    }
+
+    public static void closeDriver(){
+        /*
+        this method will make sure our driver value is always null after using quit() method
+         */
+        if (driverPool.get() != null) {
+            driverPool.get().quit(); // this will terminate the existing session. value will not even be null
+            driverPool.remove();
+        }
+
+    }
+
+}
